e920d80159a0206b08a4694cfc6a299db5fd853c
asmraihan <asmraihanctg14@gmail.com>
2024-09-17 13:53:00 +0600

    Refactor database connection setup

--- src/server/db.ts
+++ src/server/db.ts
@@ -6,6 +6,6 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
 
-await client.connect();
+// await client.connect();
 
 export const db = drizzle(client, { schema });
